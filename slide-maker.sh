430d8e7f86a2a9baea02b6c452c592a03108d047
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-04-19 22:27:49 +0100

    slight tweak to the slide size to stop vertical tiling of bg image

--- slide-maker.sh
+++ slide-maker.sh
@@ -6,4 +6,4 @@
 # Using a larger size fixes the formatting issues. Too large starts to tile
 # the background though. Not sure if these values will work universally
 # See for info: https://github.com/astefanutti/decktape/issues/151#issuecomment-456166075
-decktape reveal --max-slides 200 --size='2560x1920' $1?fragments=true $2
+decktape reveal --max-slides 200 --size='2253x1690' $1?fragments=true $2
